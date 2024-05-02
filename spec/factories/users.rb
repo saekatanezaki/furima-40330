4e81e1773f6e0434a6431a95d2f121c0d6407669
saekatanezaki <155140417+saekatanezaki@users.noreply.github.com>
2024-05-02 20:26:41 +0900

    ユーザー管理機能の修正5/2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     birthday { Faker::Date.between(from: '1930-01-01', to: Date.today) }
-    last_name { '山田' } # 任意の姓をセット
-    first_name { '太郎' } # 任意の名をセット
-    last_name_kana { 'ヤマダ' }  # 任意の姓（カナ）をセット
-    first_name_kana { 'タロウ' } # 任意の名（カナ）をセット
+    last_name { '山田' }
+    first_name { '太郎' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
   end
 end
