4c2b14f9e87fdb8d3134b62cc66b3dce20c95f03
saekatanezaki <155140417+saekatanezaki@users.noreply.github.com>
2024-05-10 20:56:59 +0900

    商品出品機能の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,53 +10,53 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       context '商品登録ができないとき' do
-        it 'item_nameが' - --'では出品できない' do
+        it 'item_nameが空では出品できない' do
           @item.item_name = ''
           @item.valid?
           expect(@item.errors.full_messages).to include "Item name can't be blank"
         end
-        it 'contentが' - --'では出品できない' do
+        it 'contentが空ときは出品できない' do
           @item.content = ''
           @item.valid?
           expect(@item.errors.full_messages).to include "Content can't be blank"
         end
-        it 'priceが' - --'では出品できない' do
+        it 'priceが空のときは出品できない' do
           @item.price = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include "Price can't be blank"
+          expect(@item.errors.full_messages).to include "Price can't br blank"
         end
-        it 'category_idが' - --'では出品できない' do
-          @item.category_id = ''
+        it 'category_idに「---」が選択されているときは出品できない' do
+          @item.category_id = '---'
           @item.valid?
-          expect(@item.errors.full_messages).to include "Category can't be blank"
+          expect(@item.errors.full_messages).to include "Category can't include ---"
         end
-        it 'status_idが' - --'では出品できない' do
-          @item.status_id = ''
+        it 'status_idに「---」が選択されているときは出品できない' do
+          @item.status_id = '---'
           @item.valid?
-          expect(@item.errors.full_messages).to include "Status can't be blank"
+          expect(@item.errors.full_messages).to include "Status can't include ---"
         end
-        it 'shipping_status_idが' - --'では出品できない' do
-          @item.shipping_status_id = ''
+        it 'shipping_status_idに「---」が選択されているときは出品できない' do
+          @item.shipping_status_id = '---'
           @item.valid?
-          expect(@item.errors.full_messages).to include "Shipping status can't be blank"
+          expect(@item.errors.full_messages).to include "Shipping status can't include ---"
         end
-        it 'prefecture_idが' - --'では出品できない' do
-          @item.prefecture_id = ''
+        it 'prefecture_idに「---」が選択されているときは出品できない' do
+          @item.prefecture_id = '---'
           @item.valid?
-          expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+          expect(@item.errors.full_messages).to include "Prefecture can't include ---"
         end
-        it 'scheduled_delivery_idが' - --'では出品できない' do
-          @item.scheduled_delivery_id = ''
+        it 'scheduled_delivery_idに「---」が選択されているときは出品できない' do
+          @item.scheduled_delivery_id = '---'
           @item.valid?
-          expect(@item.errors.full_messages).to include "Scheduled delivery can't be blank"
+          expect(@item.errors.full_messages).to include "Scheduled delivery can't include ---"
         end
         it 'priceが全角数字だと保存できないこと' do
           @item.price = '２０００'
           @item.valid?
           expect(@item.errors.full_messages).to include('Price is invalid')
         end
-        it 'priceが1円未満では保存できないこと' do
-          @item.price = 0
+        it 'priceが300円未満では保存できないこと' do
+          @item.price = 300
           @item.valid?
           expect(@item.errors.full_messages).to include('Price is invalid')
         end
@@ -64,6 +64,11 @@ RSpec.describe Item, type: :model do
           @item.price = 1_000_001
           @item.valid?
           expect(@item.errors.full_messages).to include('Price is invalid')
+        end
+        it 'ユーザーが紐付いていなければ投稿できない' do
+          @item.user = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include('User must exist')
         end
       end
     end
