15918b2f1e4e0d76179c82a4f156948ac0d9d8dc
saekatanezaki <155140417+saekatanezaki@users.noreply.github.com>
2024-05-14 20:22:44 +0900

    商品詳細編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,13 +13,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    return if current_user == @item.user
+
+    redirect_to root_path
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
-
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   resources :items
-  # resources :orders
+  resources :orders
 end
-# end
