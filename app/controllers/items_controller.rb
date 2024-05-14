2699a6441a2c50dcf88b6f5d2cb9223cba982beb
saekatanezaki <155140417+saekatanezaki@users.noreply.github.com>
2024-05-14 21:06:50 +0900

    destroy actionに条件分岐をつける

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    return unless user_signed_in? && current_user.id == @item.user_id
+
     @item.destroy
     redirect_to root_path
   end
